260b6eec6ba70917acd73b2a69bc00bc23e12c0e
mbh77 <bh.min@live.co.kr>
2026-03-18 08:05:20 +0000

    제보 제안 하기 기능 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView.vue'
 import AboutView from '../views/AboutView.vue'
+import FeedbackView from '../views/FeedbackView.vue'
+
 
 const routes = [
   { path: '/', name: 'main', component: MainView },
   { path: '/about', name: 'about', component: AboutView },
+  { path: '/feedback', name: 'feedback', component: FeedbackView },
 ]
 
 const router = createRouter({
