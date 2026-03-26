2fe966321871d802d32272dfe4d3bdd7799bc725
mbh77 <bh.min@live.co.kr>
2026-03-26 09:25:33 +0000

    자잘한 UI 조정

--- frontend/src/components/ImageGallery.vue
+++ frontend/src/components/ImageGallery.vue
@@ -1,9 +1,12 @@
 <template>
   <!-- 슬라이더 -->
-  <div v-if="images.length > 0" class="gallery-slider">
+  <div v-if="images.length > 0" class="gallery-slider"
+    @touchstart="onSwipeStart" @touchend="onSwipeEnd"
+    @mousedown="onSwipeStart" @mouseup="onSwipeEnd"
+  >
     <div class="gallery-track" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
       <div v-for="(img, idx) in images" :key="idx" class="gallery-slide" @click="openViewer(idx)">
-        <img :src="img.url" />
+        <img :src="img.url" draggable="false" />
       </div>
     </div>
     <button v-if="images.length > 1 && currentIndex > 0" class="gallery-arrow gallery-prev" @click="currentIndex--">‹</button>
@@ -31,7 +34,7 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
+const props = defineProps({
   images: { type: Array, default: () => [] },
 })
 
@@ -39,7 +42,37 @@ const currentIndex = ref(0)
 const viewerOpen = ref(false)
 const viewerIndex = ref(0)
 
+// 스와이프 처리
+let swipeStartX = 0
+let swipeStartTime = 0
+
+function getX(e) {
+  return e.touches ? e.touches[0].clientX : e.clientX
+}
+
+function onSwipeStart(e) {
+  swipeStartX = getX(e)
+  swipeStartTime = Date.now()
+}
+
+function onSwipeEnd(e) {
+  const endX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX
+  const diff = swipeStartX - endX
+  const elapsed = Date.now() - swipeStartTime
+
+  // 50px 이상 또는 빠른 스와이프(300ms 이내 30px)
+  if (Math.abs(diff) > 50 || (elapsed < 300 && Math.abs(diff) > 30)) {
+    if (diff > 0 && currentIndex.value < props.images.length - 1) {
+      currentIndex.value++
+    } else if (diff < 0 && currentIndex.value > 0) {
+      currentIndex.value--
+    }
+  }
+}
+
 function openViewer(idx) {
+  // 스와이프 후 클릭 방지: 짧은 탭만 열기
+  if (Date.now() - swipeStartTime > 300) return
   viewerIndex.value = idx
   viewerOpen.value = true
 }
